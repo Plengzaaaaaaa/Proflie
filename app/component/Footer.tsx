78729a3b25de77a5a07c9ddf13a369f626b240a8
Plengzaaaaaaa <652021067@tsu.ac.th>
2025-06-19 15:24:39 +0700

    new

---
+++ app/component/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+    <footer className="bg-gray-200 text-center p-4">
+    <p>&copy; 2025 MyApp. All rights reserved.</p>
+    </footer>
+    );
+   } 
+   
---
+++ app/component/Header.tsx
@@ -0,0 +1,12 @@
+
+
+// components/Header.tsx
+export default function Header() {
+    return (
+    <header className="bg-blue-500 text-white p-4">
+    <h1>My Next.js App</h1>
+    </header>
+    );
+   } 
+
+//    export default header()
